e423c0d3e2e7f48765a0d887ba1a0c0e39566f00
Ho Chi An <hochian31@gmail.com>
2023-12-21 19:27:45 +0700

    First Commit

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ const app = express();
 
 app.use(express.json());
 app.use('/', (req, res) => res.send('Hello World!'));
-app.use('/user', userRoute);
-app.use('/auth', authRoute);
-app.use('/country', countryRoute);
-app.use('/location', locationRoute);
-app.use('/hotel', hotelRoute);
-app.use('/review', reviewRoute);
-app.use('/room', roomRoute);
-app.use('/book', bookRoute);
+app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/country', countryRoute);
+app.use('/api/location', locationRoute);
+app.use('/api/hotel', hotelRoute);
+app.use('/api/review', reviewRoute);
+app.use('/api/room', roomRoute);
+app.use('/api/book', bookRoute);
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
